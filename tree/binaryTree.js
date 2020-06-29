4695f30187ebd8698caae9b1329de7d5d22cb3cd
yebaojia <695988558@qq.com>
2020-06-29 18:00:37 +0800

    优化

--- tree/binaryTree.js
+++ tree/binaryTree.js
@@ -7,15 +7,11 @@ function TreeNode(val) {
 // 根据数组生成二叉树
 function GenerateTreeNode(arr) {
   if (!arr || arr.length === 0) return null;
-  var len = arr.length;
-  var index = 0;
-  var head = new TreeNode(arr[index]);
+  var head = new TreeNode(arr[0]);
   var nodeQueue = [];
   nodeQueue.push(head);
   var cur;
-  while (index < len) {
-    index++;
-    if (index >= len) return head;
+  for (var index = 1; index < arr.length; index++) {
     cur = nodeQueue.shift();
     var left = arr[index];
     if (left) {
@@ -23,7 +19,6 @@ function GenerateTreeNode(arr) {
       nodeQueue.push(cur.left);
     }
     index++;
-    if (index >= len) return head;
     var right = arr[index];
     if (right) {
       cur.right = new TreeNode(right);
