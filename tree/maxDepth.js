ec63e3cb35b823303182ec0524ad307bab6486b4
yebaojia <695988558@qq.com>
2020-06-30 16:00:56 +0800

    maxDepth

--- tree/maxDepth.js
+++ tree/maxDepth.js
@@ -2,22 +2,54 @@
 var GenerateTreeNode = require('./binaryTree').GenerateTreeNode
 
 var treeNode = new GenerateTreeNode([3,9,20,null,null,15,7]);
-console.log(treeNode);
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-// var max = 0;
+
+// BFS
 // var maxDepth = function(root) {
-//   var level = 0;
-//   if (root) {
-//     dfs(root, level);
+//   if (!root) {
+//     return 0;
 //   }
-//   return max;
+//   var queue = [root];
+//   var level = 0;
+//   while (queue.length) {
+//     var len = queue.length;
+//     for (var i = 0; i < len; i++) {
+//       var node = queue.shift();
+//       if (node.left) queue.push(node.left);
+//       if (node.right) queue.push(node.right);
+//     }
+//     level++
+//   }
+//   return level;
 // };
-// var dfs = function(node, level) {
-//   max = level > max ? level : max;
-//   if (node.left) dfs(node.left, level + 1);
-//   if (node.right) dfs(node.right, level + 1);
-// }
-// maxDepth(binaryTree.root);
+
+// DFS
+// var maxDepth = function(root) {
+//   if (!root) {
+//     return 0;
+//   }
+//   var steak = [];
+//   steak.push([1, root]);
+//   var level = 0;
+//   while (steak.length) {
+//     var cur = steak.pop();
+//     var curLevel = cur[0];
+//     var node = cur[1];
+//     if (node) {
+//       level = Math.max(level, curLevel);
+//       steak.push([curLevel + 1, node.left]);
+//       steak.push([curLevel + 1, node.right]);
+//     }
+//   }
+//   return level;
+// };
+
+
+// 分治
+var maxDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+};
+
+console.log(maxDepth(treeNode))
