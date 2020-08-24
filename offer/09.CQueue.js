55b7864e3519a35c7f910936ca0a5c2f36275dea
yebaojia <695988558@qq.com>
2020-08-24 15:18:30 +0800

    用两个栈实现队列

--- offer/09.CQueue.js
+++ offer/09.CQueue.js
@@ -1,3 +1,5 @@
+// 09. 用两个栈实现队列
+
 var CQueue = function() {
   this.steak1 = [];
   this.steak2 = [];
@@ -22,6 +24,11 @@ CQueue.prototype.deleteHead = function() {
   }
   return this.steak2.pop() || -1;
 };
+/**
+ * 复杂度分析
+ * 时间复杂度：对于插入和删除操作，时间复杂度均为 O(1)。插入不多说，对于删除操作，虽然看起来是 O(n) 的时间复杂度，但是仔细考虑下每个元素只会「至多被插入和弹出 stack2 一次」，因此均摊下来每个元素被删除的时间复杂度仍为 O(1)。
+ * 空间复杂度：O(n)。需要使用两个栈存储已有的元素。
+ */
 
 /**
  * Your CQueue object will be instantiated and called as such:
