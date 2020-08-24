3fa27d762f18d06433c26b61797137769463b9f1
yebaojia <695988558@qq.com>
2020-08-24 14:39:07 +0800

    实现 Trie (前缀树)

--- main/208.trie.js
+++ main/208.trie.js
@@ -1,3 +1,4 @@
+// 208. 实现 Trie (前缀树)
 
 var TrieNode = function() {
   this.next = {};
@@ -30,6 +31,11 @@ Trie.prototype.insert = function(word) {
   node.isEnd = true;
   return node.isEnd;
 };
+/**
+ * 复杂度分析
+ * 时间复杂度：O(m)，其中 mm 为键长。在算法的每次迭代中，我们要么检查要么创建一个节点，直到到达键尾。只需要 m 次操作。
+ * 空间复杂度：O(m)。最坏的情况下，新插入的键和 Trie 树中已有的键没有公共前缀。此时需要添加 m 个结点，使用 O(m) 空间。
+ */
 
 /**
  * Returns if the word is in the trie. 
@@ -50,6 +56,11 @@ Trie.prototype.search = function(word) {
   }
   return node.isEnd;
 };
+/**
+ * 复杂度分析
+ * 时间复杂度 : O(m)。算法的每一步均搜索下一个键字符。最坏的情况下需要 m 次操作。
+ * 空间复杂度 : O(1)。
+ */
 
 /**
  * Returns if there is any word in the trie that starts with the given prefix. 
@@ -70,6 +81,11 @@ Trie.prototype.startsWith = function(prefix) {
   }
   return true;
 };
+/**
+ * 复杂度分析
+ * 时间复杂度 : O(m)。
+ * 空间复杂度 : O(1)。
+ */
 
 /**
  * Your Trie object will be instantiated and called as such:
