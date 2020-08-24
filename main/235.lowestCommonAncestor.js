7e7f116306a2561c5e52ede197f3adb20f0a810f
yebaojia <695988558@qq.com>
2020-08-24 10:48:26 +0800

    二叉搜索树的最近公共祖先

---
+++ main/235.lowestCommonAncestor.js
@@ -0,0 +1,18 @@
+// 二叉搜索树的最近公共祖先
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    
+};
