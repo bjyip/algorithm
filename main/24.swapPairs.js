87d61315c4f000b319bc95ec1fef0385ce3ad1fb
yebaojia <695988558@qq.com>
2020-08-24 09:50:08 +0800

    两两交换链表中的节点

--- main/24.swapPairs.js
+++ main/24.swapPairs.js
@@ -1,5 +1,5 @@
-var LinkList = require('./linkList').LinkList
-var ListNode = require('./linkList').ListNode
+var LinkList = require('../linkList/linkList').LinkList
+var ListNode = require('../linkList/linkList').ListNode
 var linkList = new LinkList();
 linkList.appendBatch([1, 2, 3, 4, 5])
 // linkList.delete(1)
@@ -7,15 +7,20 @@ linkList.appendBatch([1, 2, 3, 4, 5])
 // console.log(linkList.get(1))
 
 // 递归
-// var swapPairs = function(head) {
-//   if (!head || !head.next) {
-//     return head;
-//   }
-//   var firstNode = head, secondNode = head.next;
-//   firstNode.next = swapPairs(secondNode.next)
-//   secondNode.next = firstNode
-//   return secondNode;
-// };
+var swapPairs = function(head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  var firstNode = head, secondNode = head.next;
+  firstNode.next = swapPairs(secondNode.next)
+  secondNode.next = firstNode
+  return secondNode;
+};
+/**
+ * 复杂度分析
+ * 时间复杂度：O(N)，其中 N 指的是链表的节点数量。
+ * 空间复杂度：O(N)，递归过程使用的堆栈空间。
+ */
 
 // 迭代
 var swapPairs = function(head) {
@@ -35,4 +40,9 @@ var swapPairs = function(head) {
   }
   return dummy.next
 }
+/**
+ * 复杂度分析
+ * 时间复杂度：O(N)，其中 N 指的是链表的节点数量。
+ * 空间复杂度：O(1)。
+ */
 swapPairs(linkList.head);
