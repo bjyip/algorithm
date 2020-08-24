1b05645e25e3f6a92a4ef7c044aafcfe4a901652
yebaojia <695988558@qq.com>
2020-08-24 15:34:11 +0800

    调整数组顺序使奇数位于偶数前面

--- offer/21.exchange.js
+++ offer/21.exchange.js
@@ -1,3 +1,5 @@
+// 21.调整数组顺序使奇数位于偶数前面
+
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -13,5 +15,10 @@ var exchange = function(nums) {
   }
   return nums;
 };
+/**
+ * 复杂度分析：
+ * 时间复杂度 O(N) ： N 为数组 nums 长度，双指针 i, j 共同遍历整个数组。
+ * 空间复杂度 O(1) ： 双指针 i, j 使用常数大小的额外空间。
+ */
 
 console.log(exchange([1,2,3,4]))
