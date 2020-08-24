7c029de25da5d0e047af5166cb2f948f79f3035d
yebaojia <695988558@qq.com>
2020-08-24 15:15:56 +0800

    二维数组中的查找

--- offer/04.findNumberIn2DArray.js
+++ offer/04.findNumberIn2DArray.js
@@ -1,3 +1,6 @@
+// 04. 二维数组中的查找
+
+// 线性查找
 var findNumberIn2DArray = function(matrix, target) {
   if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
     return false;
@@ -15,3 +18,8 @@ var findNumberIn2DArray = function(matrix, target) {
   }
   return false
 };
+/**
+ * 复杂度分析
+ * 时间复杂度：O(n+m)。访问到的下标的行最多增加 n 次，列最多减少 m 次，因此循环体最多执行 n + m 次。
+ * 空间复杂度：O(1)。
+ */
