4e11dfc9a3fd6c18767e7398dd5019ec18da5ba2
yebaojia <695988558@qq.com>
2020-08-24 15:41:53 +0800

    对称的二叉树

--- offer/28.isSymmetric.js
+++ offer/28.isSymmetric.js
@@ -1,4 +1,6 @@
-var GenerateTreeNode = require('./binaryTree').GenerateTreeNode
+// 28.对称的二叉树
+
+var GenerateTreeNode = require('../tree/binaryTree').GenerateTreeNode
 
 var treeNode = new GenerateTreeNode([1,2,2,3,4,4,3]);
 
@@ -22,5 +24,10 @@ var recur = function(left, right) {
   if (!left || !right || left.val !== right.val) return false;
   return recur(left.left, right.right) && recur(left.right, right.left);
 }
+/**
+ * 复杂度分析：
+ * 时间复杂度 O(N) ： 其中 N 为二叉树的节点数量，每次执行 recur() 可以判断一对节点是否对称，因此最多调用 N/2 次 recur() 方法。
+ * 空间复杂度 O(N) ： 最差情况下（见下图），二叉树退化为链表，系统使用 O(N) 大小的栈空间。
+ */
 
 console.log(isSymmetric(treeNode));
