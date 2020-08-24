6ed3c3af4399a213c22dc2008842aa98fe7b8cfd
yebaojia <695988558@qq.com>
2020-08-24 15:37:27 +0800

    合并两个排序的链表

--- offer/25.mergeTwoLists.js
+++ offer/25.mergeTwoLists.js
@@ -1,10 +1,13 @@
 
-var LinkList = require('./linkList').LinkList
+
+// 25.合并两个排序的链表
+
+var LinkList = require('../linkList/linkList').LinkList
 var linkList1 = new LinkList();
 var linkList2 = new LinkList();
 linkList1.appendBatch([1, 2, 4])
 linkList2.appendBatch([1, 3, 4])
-var ListNode = require('./linkList').ListNode
+var ListNode = require('../linkList/linkList').ListNode
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -32,4 +35,9 @@ var mergeTwoLists = function(l1, l2) {
   cur.next = l1 ? l1 : l2;
   return dum.next;
 };
+/**
+ * 复杂度分析：
+ * 时间复杂度 O(M+N) ： M, N 分别为链表 l1, l2 的长度，合并操作需遍历两链表。
+ * 空间复杂度 O(1)O(1) ： 节点引用 dum , cur 使用常数大小的额外空间。
+ */
 console.log(mergeTwoLists(linkList1.head, linkList2.head))
