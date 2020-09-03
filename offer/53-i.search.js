cb0ffaaccea6d7e8024a8510216ce176b8b2afb2
yebaojia <695988558@qq.com>
2020-09-03 19:30:10 +0800

    在排序数组中查找数字

---
+++ offer/53-i.search.js
@@ -0,0 +1,23 @@
+/**
+ * 53 - I. 在排序数组中查找数字
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// 二分法
+var helper = function(nums, target) {
+  var i = 0, j = nums.length - 1;
+  while (i <= j) {
+    var m = Math.floor((i + j) / 2);
+    if (nums[m] <= target) {
+      i = m + 1;
+    } else {
+      j = m - 1;
+    }
+  }
+  return i;
+}
+var search = function(nums, target) {
+ return helper(nums, target) - helper(nums, target - 1);
+};
+console.log(search([5,7,7,8,8,8,10], 8))
