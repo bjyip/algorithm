1051039cddb5dc9aa4e3e7dd24151d53bebc5a0a
yebaojia <695988558@qq.com>
2021-06-28 17:16:05 +0800

    和为s的两个数字

--- offer/57.twoSum.js
+++ offer/57.twoSum.js
@@ -1,5 +1,6 @@
 /**
  * 57.和为s的两个数字
+ * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -9,14 +10,15 @@ var twoSum = function(nums, target) {
   var l = 0, r = nums.length - 1, s = 0;
   while (l < r) {
     s = nums[l] + nums[r];
-    if (target > s) {
+    if (s < target) {
       l++;
-    } else if (target < s) {
+    } else if (s > target) {
       r--;
     } else {
       return [nums[l], nums[r]];
     }
   }
+  return null
 };
 /**
  * 复杂度分析：
