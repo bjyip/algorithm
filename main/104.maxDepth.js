2f91e06259e1bbfc497bfb5097595f292883e12c
yebaojia <695988558@qq.com>
2021-09-03 09:18:31 +0800

    maxDepth

--- main/104.maxDepth.js
+++ main/104.maxDepth.js
@@ -29,6 +29,21 @@ var maxDepth = function(root) {
  * 空间复杂度：此方法空间的消耗取决于队列存储的元素数量，其在最坏情况下会达到 O(n)。
  */
 
+
+// DFS（递归、后序遍历）/分治
+var maxDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+/**
+ * 复杂度分析
+ * 时间复杂度：O(n)，其中 n 为二叉树节点的个数。每个节点在递归中只被遍历一次。
+ * 空间复杂度：O(height)，其中 height 表示二叉树的高度。递归函数需要栈空间，而栈空间取决于递归的深度，因此空间复杂度等价于二叉树的高度。
+ */
+
+
 // DFS（栈、前序遍历）
 var maxDepth = function(root) {
   if (!root) {
@@ -58,17 +73,5 @@ var maxDepth = function(root) {
  * 空间复杂度：此方法空间的消耗取决于队列存储的元素数量，其在最坏情况下会达到 O(n)。
  */
 
-// DFS（递归、后序遍历）/分治
-var maxDepth = function(root) {
-  if (!root) {
-    return 0;
-  }
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-};
-/**
- * 复杂度分析
- * 时间复杂度：O(n)，其中 n 为二叉树节点的个数。每个节点在递归中只被遍历一次。
- * 空间复杂度：O(height)，其中 height 表示二叉树的高度。递归函数需要栈空间，而栈空间取决于递归的深度，因此空间复杂度等价于二叉树的高度。
- */
 
 console.log(maxDepth(treeNode))
