db1e6c3ac29c08a28285c43ceee1dbe9cb811796
yebaojia <695988558@qq.com>
2021-09-03 11:28:07 +0800

    minDepth

--- main/111.minDepth.js
+++ main/111.minDepth.js
@@ -28,8 +28,31 @@ var minDepth = function(root) {
  * 复杂度分析
  * 时间复杂度：O(N)，其中 N 是树的节点数。对每个节点访问一次。
  * 空间复杂度：O(N)，其中 N 是树的节点数。空间复杂度主要取决于队列的开销，队列中的元素个数不会超过树的节点数。
-
  */
+
+// DFS递归
+var minDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left && !root.right) {
+    return 1;
+  }
+  var level = Infinity;
+  if (root.left) {
+    level = Math.min(minDepth(root.left), level);
+  }
+  if (root.right) {
+    level = Math.min(minDepth(root.right), level);
+  }
+  return level + 1;
+};
+/**
+ * 复杂度分析
+ * 时间复杂度：O(n)，其中 n 为二叉树节点的个数。每个节点在递归中只被遍历一次。
+ * 空间复杂度：O(height)，其中 height 表示二叉树的高度。递归函数需要栈空间，而栈空间取决于递归的深度，因此空间复杂度等价于二叉树的高度。
+ */
+
 
 // DFS遍历
 var minDepth = function(root) {
@@ -60,23 +83,6 @@ var minDepth = function(root) {
  * 空间复杂度：O(H)，其中 H 是树的高度。空间复杂度主要取决于递归时栈空间的开销，最坏情况下，树呈现链状，空间复杂度为 O(N)。平均情况下树的高度与节点数的对数正相关，空间复杂度为 O(logN)。
  */
 
-// DFS递归
-var minDepth = function(root) {
-  if (!root) {
-    return 0;
-  }
-  if (!root.left && !root.right) {
-    return 1;
-  }
-  var level = Infinity;
-  if (root.left) {
-    level = Math.min(minDepth(root.left), level);
-  }
-  if (root.right) {
-    level = Math.min(minDepth(root.right), level);
-  }
-  return level + 1;
-};
 
 // 分治
 var minDepth = function(root) {
