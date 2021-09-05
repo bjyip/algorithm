e9845c830a054867eedb861494d52d41f37fe118
yebaojia <695988558@qq.com>
2021-09-05 19:07:17 +0800

    fix

--- other/call.js
+++ other/call.js
@@ -5,7 +5,7 @@ Function.prototype.myCall = function(context, ...args) {
   context = context || window        // 若没有传入this, 默认绑定window对象
   const fn = Symbol('fn')            // 声明一个独有的Symbol属性, 防止fn覆盖已有属性
   context[fn] = this                 // this指向当前调用call的函数对象（Function实例），即我们要改变this指向的函数
-  const result = context.fn(...args) // 执行当前函数，隐式绑定，当前函数的this指向context
+  const result = context[fn](...args) // 执行当前函数，隐式绑定，当前函数的this指向context
   delete context[fn]                 // 删除我们声明的fn属性
   return result                      // 返回函数执行结果
 }
