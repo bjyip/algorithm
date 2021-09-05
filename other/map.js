fc50438dbc974b56ca55e6adc8cc297d825087e0
yebaojia <695988558@qq.com>
2021-09-05 23:36:52 +0800

    fix map

--- other/map.js
+++ other/map.js
@@ -1,16 +1,13 @@
-
-
 Array.prototype.myMap = function(fn, context) {
-  if (typeof this !== 'array') {
+  if (!(this instanceof Array)) {
     throw TypeError(this + '.myMap is not a function')
   }
-  const temp = []
+  const res = []
   for (let i = 0; i < this.length; i++) { // this指向调用myMap函数的数组
     // 如果 context 参数提供给myMap，则会被用作回调函数的this值。否则undefined会被用作回调函数的this值
-    const result = fn.call(context, this[i], i, this)
-    temp.push(result)
+    res.push(fn.call(context, this[i], i, this))
   }
-  return temp
+  return res
 }
 // 原理
 // 1. 在原型上添加一个方法
