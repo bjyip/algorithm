d6d063b12e69c64dedde98ce225e80c29455927c
yebaojia <695988558@qq.com>
2021-09-05 23:29:52 +0800

    fix each

--- other/forEach.js
+++ other/forEach.js
@@ -1,12 +1,13 @@
 Array.prototype.myForEach = function(fn, context) {
-  if (typeof this !== 'array') {
+  if (!(this instanceof Array)) {
     throw TypeError(this + '.myMap is not a function')
   }
   // this指向调用myMap函数的数组
   for (let i = 0; i < this.length; i++) {
     // 如果 context 参数有值，则每次 callback 函数被调用时，this 都会指向 context 参数。
     // 如果省略了 context 参数，或者其值为 null 或 undefined，this 则指向全局对象。
-    fn.call(context || window, this[i], i, context)    
+    // fn的3个参数分别是：item， index， array
+    fn.call(context || window, this[i], i, this)
   }
 }
 
