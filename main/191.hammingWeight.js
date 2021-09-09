3b7ba1cc319899b11764920e62ab600356f3b181
yebaojia <695988558@qq.com>
2021-09-09 23:46:39 +0800

    hammingWeight

--- main/191.hammingWeight.js
+++ main/191.hammingWeight.js
@@ -1,18 +1,19 @@
 // 191. 位1的个数
 // 编写一个函数，输入是一个无符号整数（以二进制串的形式），
 // 返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+// 输入必须是长度为 32 的 二进制串。
 
 // 循环和位移动
 var hammingWeight = function(n) {
-  var bits = 0;
-  var mask = 1;
+  var ret = 0;
+  var mask = 1; // 转成二进制是：00000000 00000000 00000000 0000001
   for (var i = 0; i < 32; i++) {
-    if ((n & mask) !== 0) {
-      bits++;
+    if ((n & mask) !== 0) { // 二进制中 1 & 1等于1，1 & 0等于0，0 & 0等于0
+      ret++;
     }
     mask <<= 1;
   }
-  return bits;
+  return ret;
 };
 /**
  * 复杂度分析
@@ -22,12 +23,12 @@ var hammingWeight = function(n) {
 
 // 位操作的小技巧
 var hammingWeight = function(n) {
-  var bits = 0;
+  var ret = 0;
   while (n !== 0) {
-    bits++;
-    n &= (n - 1);
+    ret++;
+    n &= (n - 1); // n & (n - 1) => 清零最低位的1
   }
-  return bits;
+  return ret;
 }
 /**
  * 复杂度分析
@@ -38,11 +39,11 @@ var hammingWeight = function(n) {
 
 // 方法三，js中有问题
 var hammingWeight = function(n) {
-  var bits = 0;
+  var ret = 0;
   while (n !== 0) {
-    bits += n & 1;
+    ret += n & 1;
     n >>= 1;
   }
-  return bits;
+  return ret;
 }
 console.log(hammingWeight('11111111111111111111111111111101'))
