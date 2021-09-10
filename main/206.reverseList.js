5bf756ba224b17a0040082d6696b2d8a467caea7
yebaojia <695988558@qq.com>
2021-09-10 22:41:20 +0800

    reverseList!

--- main/206.reverseList.js
+++ main/206.reverseList.js
@@ -4,29 +4,33 @@ var linkList = new LinkList();
 linkList.appendBatch([1, 2, 3, 4, 5])
 
 // 双指针
+// 假设链表为 1→2→3→∅，我们想要把它改成 ∅←1←2←3。
+// 在遍历链表时，将当前节点的 next 指针改为指向前一个节点。
+// 由于节点没有引用其前一个节点，因此必须事先存储其前一个节点。
+// 在更改引用之前，还需要存储后一个节点。最后返回新的头引用。
 var reverseList = function(head) {
   if (!head) {
-      return head;
+    return head;
   }
   var curr = head, prev = null, tempNext = null;
   while (curr) {
-      tempNext = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = tempNext;
+    tempNext = curr.next; // 暂存断掉的部分
+    curr.next = prev;
+    prev = curr;
+    curr = tempNext; // 赋值断掉的部分，继续下一次迭代
   }
-  return prev;
+  return prev; // 最后cur未null时跳出while，故返回pre
 };
 /**
  * 复杂度分析
- * 时间复杂度：O(n)，假设 n 是列表的长度，时间复杂度是 O(n)。
+ * 时间复杂度：O(n)，其中 n 是链表的长度。需要遍历链表一次。
  * 空间复杂度：O(1)。
  */
 
-// 递归
+// 递归（未解决）
 var reverseList = function(head) {
   if (!head || !head.next) {
-      return head;
+    return head;
   }
   var p = reverseList(head.next);
   head.next.next = head;
