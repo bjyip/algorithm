edcb73a508497d325dd933211e13aa55c400b242
yebaojia <695988558@qq.com>
2021-09-11 15:13:09 +0800

    isPowerOfTwo

--- main/231.isPowerOfTwo.js
+++ main/231.isPowerOfTwo.js
@@ -1,8 +1,10 @@
 // 2的幂
+// 给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
+// 如果存在一个整数 x 使得 n == 2^x ，则认为 n 是 2 的幂次方。
 
 // 迭代
 var isPowerOfTwo = function(n) {
-  if (n === 0) {
+  if (n <= 0) {
     return false;
   }
   while (n % 2 === 0) {
@@ -17,6 +19,7 @@ var isPowerOfTwo = function(n) {
  */
 
 // 位运算：获取二进制中最右边的 1
+// 一个数 n 是 2 的幂，当且仅当 n 是正整数，并且 n 的二进制表示中仅包含 1 个 1。
 var isPowerOfTwo = function(n) {
   if (n <= 0) {
     return false;
@@ -30,6 +33,7 @@ var isPowerOfTwo = function(n) {
  */
 
 // 位运算：去除二进制中最右边的 1
+// 一个数 n 是 2 的幂，当且仅当 n 是正整数，并且 n 的二进制表示中仅包含 1 个 1。
 var isPowerOfTwo = function(n) {
   if (n <= 0) {
     return false;
