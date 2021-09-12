aac3d1c0b8886b4bd6df210a51fec7a8333678b7
yebaojia <695988558@qq.com>
2021-09-12 15:07:30 +0800

    findNumberIn2DArray

--- offer/04.findNumberIn2DArray.js
+++ offer/04.findNumberIn2DArray.js
@@ -19,21 +19,21 @@
 // 线性查找
 var findNumberIn2DArray = function(matrix, target) {
   if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
-    return false;
+    return false
   }
-  var rows = matrix.length, cols = matrix[0].length;
-  var row = 0, col = cols - 1;
+  var rows = matrix.length, cols = matrix[0].length
+  var row = 0, col = cols - 1
   while (row < rows && col >= 0) {
-    if (matrix[row][col] === target) {
-      return true;
-    } else if (matrix[row][col] > target) {
-      col--;
-    } else {
+    if (matrix[row][col] < target) {
       row++
+    } else if (matrix[row][col] > target) {
+      col--
+    } else {
+      return true
     }
   }
   return false
-};
+}
 /**
  * 复杂度分析
  * 时间复杂度：O(n+m)。访问到的下标的行最多增加 n 次，列最多减少 m 次，因此循环体最多执行 n + m 次。
