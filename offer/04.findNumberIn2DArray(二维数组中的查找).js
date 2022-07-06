24087fa5d2e3c95fe0ae0b7a6fc17c63f9dfb5f2
v-xbaojia <695988558@qq.com>
2022-07-06 14:50:16 +0800

    findNumberIn2DArray

--- offer/04.findNumberIn2DArray(二维数组中的查找).js
+++ offer/04.findNumberIn2DArray(二维数组中的查找).js
@@ -21,9 +21,8 @@ var findNumberIn2DArray = function(matrix, target) {
   if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
     return false
   }
-  var rows = matrix.length, cols = matrix[0].length
-  var row = 0, col = cols - 1
-  while (row < rows && col >= 0) {
+  var row = 0, col = matrix[0].length - 1
+  while (row < matrix.length && col >= 0) {
     if (matrix[row][col] < target) {
       row++
     } else if (matrix[row][col] > target) {
