6e597e833cfcf699ed9011a5d6210ccc8184940a
v-xbaojia <695988558@qq.com>
2022-07-08 17:11:38 +0800

    解析

--- other/自执行函数打印.js
+++ other/自执行函数打印.js
@@ -7,3 +7,27 @@ var name = 'weihui';
     console.log('Hello' + name);
   }
 })()
+
+
+
+
+
+
+/**
+ * 解析：
+ * 1.匿名函数里的var name = 'bigo'会变量提升，故typeof name === 'undefined'成立。
+ * 2.此处使用自执行匿名函数是为了让里面的代码存在函数作用域，从而可以变量提升，而不是为了让自执行函数提升。
+ * 3.用function来声明变量才会提升，如果外面包着自执行函数就会又提升又立即执行。
+ * 4.题目中的匿名函数只是用来混淆视听，可以看作普通函数：
+ */
+// 
+// var name = 'weihui';
+// function fn() {
+//   if (typeof name === 'undefined') {
+//     var name = 'bigo';
+//     console.log('good' + name);
+//   } else {
+//     console.log('Hello' + name);
+//   }
+// }
+// fn()
