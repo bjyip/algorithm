bcd70e02205a0ea2fee28af5487b0511470636b8
v-xbaojia <695988558@qq.com>
2023-03-10 14:10:58 +0800

    promise

--- other/简易版promise.js
+++ other/简易版promise.js
@@ -12,20 +12,20 @@ function MyPromise(fn) {
     if (_this.state === 'pending') {
       _this.state = 'fulfilled'
       _this.value = value
-      // 成功后把放到数组中的回调执行,并传入成功的值
+      // 成功后遍历执行then数组中的回调函数,并传入成功的值
       _this.resolvedCallbacks.forEach(cb => cb(_this.value))
     }
   }
-  // 私有函数：将promise的状态从pending更改为rejected，并且以value为参数依次调用then方法中注册的回调
+  // 私有函数：将promise的状态从pending更改为rejected，并且以value为参数依次调用catch方法中注册的回调
   function reject(value) {
     if (_this.state === 'pending') {
       _this.state = 'rejected'
       _this.value = value
-      // 失败后把放到数组中的回调执行,并传入失败的原因
+      // 失败后遍历执行catch数组中的回调函数,并传入失败的原因
       _this.rejectedCallbacks.forEach(cb => cb(_this.value))
     }
   }
-  // 执行 Promise 中传入的函数，并入参两个私有方法给实例使用
+  // 执行 MyPromise 中传入的函数，并入参两个私有方法给函数使用
   try {
     fn(resolve, reject)
   } catch (e) {
@@ -39,6 +39,7 @@ MyPromise.prototype.then = function(onFulfilled, onRejected) {
   // promise实例执行then函数的时候传入两个回调函数
   onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
   onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err }
+
   // 如果当前状态是pending，则把两个回调函数加入对应的数组中
   if (_this.state === 'pending') {
     _this.resolvedCallbacks.push(onFulfilled)
@@ -60,11 +61,11 @@ MyPromise.prototype.catch = function(failCallback) {
   return this.then(undefined, failCallback)
 }
 
-// promise实例的finally方法
-// 不管成功还是失败都要执行finally方法。
+// promise实例的finally方法，不管成功还是失败都要执行finally方法。
 // 在finally方法要返回一个promise对象并且状态与之前调用的Promise对象一样。
 // 调用finally传入的callback函数，callback不论返回什么，都转换为Promise对象，并且与当前调用对象的状态值是一样的。
 MyPromise.prototype.finally = function(callback) {
+  // 利用then的第一个参数实现成功的回调，then的第二个参数实现失败的回调
   return this.then(data => {
     return MyPromise.resolve(callback()).then(() => data)
   }, err => {
